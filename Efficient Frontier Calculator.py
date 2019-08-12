a3ad703d4cd450875ac3b218b6cf91543094c1cf
DariusTheProgrammer <54011006+DariusTheProgrammer@users.noreply.github.com>
2019-08-11 19:35:24 -0600

    Add files via upload

---
+++ Efficient Frontier Calculator.py
@@ -0,0 +1,84 @@
+# import needed modules
+from pandas import read_csv
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+series = read_csv('ETF Data.csv', header=0, index_col=0, squeeze=True)
+
+# calculate daily and annual returns of the assets
+returns_daily = series.pct_change()
+returns_annual = returns_daily.mean() * 250
+
+# get daily and covariance of returns of the assets
+cov_daily = returns_daily.cov()
+cov_annual = cov_daily * 250
+
+# empty lists to store returns, volatility and weights of imaginary portfolios
+port_returns = []
+port_volatility = []
+sharpe_ratio = []
+stock_weights = []
+
+# set the number of combinations for imaginary portfolios
+num_assets = len(selected)
+num_portfolios = 150000
+
+#set random seed for reproduction's sake
+np.random.seed(101)
+
+# populate the empty lists with each portfolios returns,risk and weights
+for single_portfolio in range(num_portfolios):
+    weights = np.random.random(num_assets)
+    weights /= np.sum(weights)
+    returns = np.dot(weights, returns_annual)
+    volatility = np.sqrt(np.dot(weights.T, np.dot(cov_annual, weights)))
+    sharpe = returns / volatility
+    sharpe_ratio.append(sharpe)
+    port_returns.append(returns)
+    port_volatility.append(volatility)
+    stock_weights.append(weights)
+
+# a dictionary for Returns and Risk values of each portfolio
+portfolio = {'Returns': port_returns,
+             'Volatility': port_volatility,
+             'Sharpe Ratio': sharpe_ratio}
+
+# extend original dictionary to accomodate each ticker and weight in the portfolio
+for counter,symbol in enumerate(selected):
+    portfolio[symbol+' Weight'] = [Weight[counter] for Weight in stock_weights]
+
+# make a nice dataframe of the extended dictionary
+df = pd.DataFrame(portfolio)
+
+# get better labels for desired arrangement of columns
+column_order = ['Returns', 'Volatility', 'Sharpe Ratio'] + [stock+' Weight' for stock in selected]
+
+# reorder dataframe columns
+df = df[column_order]
+
+
+# find min Volatility & max sharpe values in the dataframe (df)
+min_volatility = df['Volatility'].min()
+max_sharpe = df['Sharpe Ratio'].max()
+
+# use the min, max values to locate and create the two special portfolios
+sharpe_portfolio = df.loc[df['Sharpe Ratio'] == max_sharpe]
+min_variance_port = df.loc[df['Volatility'] == min_volatility]
+
+#showing stats of highest sharpe value portfolio & min variance portfolio
+print (sharpe_portfolio)
+print (min_variance_port)
+
+
+# plot frontier, max sharpe & min Volatility values with a scatterplot
+plt.style.use('seaborn-dark')
+df.plot.scatter(x='Volatility', y='Returns', c='Sharpe Ratio',
+                cmap='RdYlGn', edgecolors='black', figsize=(10, 8), grid=True)
+plt.scatter(x=sharpe_portfolio['Volatility'], y=sharpe_portfolio['Returns'], c='red', marker='D', s=100)
+plt.scatter(x=min_variance_port['Volatility'], y=min_variance_port['Returns'], c='blue', marker='D', s=100 )
+plt.xlabel('Volatility (Std. Deviation)')
+plt.ylabel('Expected Returns')
+plt.title('Efficient Frontier')
+plt.show()
